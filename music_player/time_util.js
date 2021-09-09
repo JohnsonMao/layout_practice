530a1ecf23fcddb129ef3168809acc2f07e87383
JohnsonMao <86179381+JohnsonMao@users.noreply.github.com>
2021-09-09 23:50:53 +0800

    update time_util.js

--- music_player/time_util.js
+++ music_player/time_util.js
@@ -1,19 +1,9 @@
 const formatSeconds = ( val ) => {
-  let sec = parseInt( val );
-  let min = 0;
-  let hour = 0;
-  let result = "";
-  if( val > 60 ) {
-    sec = parseInt( val % 60 );
-    min = parseInt( val / 60 );
-
-    if( min > 60 ) {
-      min = parseInt( min % 60);
-      hour = parseInt( min / 60);
-    }
-  }
-  if( hour > 0) result += `${hour} : `
-  if(min > 0) result += `${min} : `
-  result += `${sec}`;
-  return result;
+  const time = parseInt( val );
+  const sec = Math.floor( time % 60 );
+  const min = Math.floor( time / 60 );
+  return `
+  ${min < 10 ? '0' + min : min} : 
+  ${sec < 10 ? '0' + sec : sec}
+  `;
 }
