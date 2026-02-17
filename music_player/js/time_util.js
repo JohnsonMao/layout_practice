a364348832f948e33715333d9cd7eec2881bf17d
JohnsonMao <tutelary.maomao@gmail.com>
2026-02-17 13:26:43 +0800

    reset: empty project

--- music_player/js/time_util.js
+++
@@ -1,9 +0,0 @@
-const formatSeconds = ( val ) => {
-  const time = parseInt( val );
-  const sec = Math.floor( time % 60 );
-  const min = Math.floor( time / 60 );
-  return `
-  ${min < 10 ? '0' + min : min} : 
-  ${sec < 10 ? '0' + sec : sec}
-  `;
-}
